ed7f91fb24ba99338944d09e9197692bb26b2676
Carlos Felipe Araujo <carlosxfelipe@gmail.com>
2025-01-23 23:18:30 -0300

    Refactor app structure: move App component to src directory and simplify implementation

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import {StatusBar, Text, useColorScheme, View} from 'react-native';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+
+function App(): React.JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <View>
+      <StatusBar
+        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+        backgroundColor={backgroundStyle.backgroundColor}
+      />
+      <Text>Olá, mundo!</Text>
+    </View>
+  );
+}
+
+export default App;
