1c8ebda722a64e475eb51a149178a74b65f1e0e0
Carlos Felipe Araujo <carlosxfelipe@gmail.com>
2025-01-25 00:11:41 -0300

    Add PosterCarousel component and integrate it into HomeScreen

--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -1,9 +1,27 @@
 import {Text, View} from 'react-native';
+import {ScrollView} from 'react-native-gesture-handler';
+import {useMovies} from '../../hooks/useMovies';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import {PosterCarousel} from '../../components/movies/PosterCarousel';
 
 export const HomeScreen: React.FC = () => {
+  const {top} = useSafeAreaInsets();
+
+  const {isLoading, nowPlaying} = useMovies();
+
+  if (isLoading) {
+    return (
+      <View>
+        <Text>Carregando...</Text>
+      </View>
+    );
+  }
+
   return (
-    <View>
-      <Text>HomeScreen</Text>
-    </View>
+    <ScrollView>
+      <View style={{marginTop: top + 20, paddingBottom: 30}}>
+        <PosterCarousel movies={nowPlaying} />
+      </View>
+    </ScrollView>
   );
 };
