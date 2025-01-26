0e8f95b4b330318e43e6f6efb0f1527be6ee2dd9
Carlos Felipe Araujo <carlosxfelipe@gmail.com>
2025-01-25 23:48:23 -0300

    Integrate HorizontalCarousel for popular movies and adjust DynamicPosterCarousel display based on orientation

--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -1,14 +1,16 @@
-import {Text, View} from 'react-native';
+import {Text, useWindowDimensions, View} from 'react-native';
 import {ScrollView} from 'react-native-gesture-handler';
 import {useMovies} from '../../hooks/useMovies';
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
 // import {PosterCarousel} from '../../components/movies/PosterCarousel';
 import {DynamicPosterCarousel} from '../../components/movies/DynamicPosterCarousel';
+import {HorizontalCarousel} from '../../components/movies/HorizontalCarousel';
 
 export const HomeScreen: React.FC = () => {
   const {top} = useSafeAreaInsets();
+  const {width, height} = useWindowDimensions();
 
-  const {isLoading, nowPlaying} = useMovies();
+  const {isLoading, nowPlaying, popular} = useMovies();
 
   if (isLoading) {
     return (
@@ -18,11 +20,14 @@ export const HomeScreen: React.FC = () => {
     );
   }
 
+  const isLandscape = width > height;
+
   return (
     <ScrollView>
       <View style={{marginTop: top + 20, paddingBottom: 30}}>
         {/* <PosterCarousel movies={nowPlaying} /> */}
-        <DynamicPosterCarousel movies={nowPlaying} />
+        {!isLandscape && <DynamicPosterCarousel movies={nowPlaying} />}
+        <HorizontalCarousel movies={popular} title="Filmes mais populares" />
       </View>
     </ScrollView>
   );
