5cbb63011037c1cf7b0c406398b143cddef96f9c
Carlos Felipe Araujo <carlosxfelipe@gmail.com>
2025-01-26 20:07:41 -0300

    Implement DetailsScreen to display movie information and loading state

--- src/presentation/screens/details/DetailsScreen.tsx
+++ src/presentation/screens/details/DetailsScreen.tsx
@@ -1,9 +1,32 @@
-import {Text, View} from 'react-native';
+// import {useRoute} from '@react-navigation/native';
+import {StackScreenProps} from '@react-navigation/stack';
+import {RootStackParams} from '../../navigation/Navigation';
+import {useMovie} from '../../hooks/useMovie';
+import {FullScreenLoader} from '../../components/loaders/FullScreenLoader';
+import {ScrollView} from 'react-native-gesture-handler';
+import {MovieHeader} from '../../components/movie/MovieHeader';
+import {MovieDetails} from '../../components/movie/MovieDetails';
 
-export const DetailsScreen: React.FC = () => {
+interface Props extends StackScreenProps<RootStackParams, 'Details'> {}
+
+export const DetailsScreen = ({route}: Props) => {
+  // const {movieId} = useRoute().params as any;
+  const {movieId} = route.params;
+  const {isLoading, movie, cast = []} = useMovie(movieId);
+
+  if (isLoading) {
+    return <FullScreenLoader />;
+  }
+
   return (
-    <View>
-      <Text>DetailsScreen</Text>
-    </View>
+    <ScrollView>
+      <MovieHeader
+        originalTitle={movie!.originalTitle}
+        title={movie!.title}
+        poster={movie!.poster}
+      />
+
+      <MovieDetails movie={movie!} cast={cast} />
+    </ScrollView>
   );
 };
